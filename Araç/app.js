7983c77d7c155c89bee84aa6b7e69bb35f19846e
Fuat Keleş <kelesfuat.96@gmail.com>
2022-03-01 22:27:04 +0300

    final commit

--- Araç/app.js
+++ Araç/app.js
@@ -11,6 +11,8 @@ const fiyat = document.getElementById("fiyat");
 const ekle = document.getElementById("ekle");
 //Form içindeki Div'ler
 const formGroup = document.querySelectorAll(".form-group");
+//Div oluşturmak için gövde
+const body = document.querySelector(".card-body");
 
 //Tek tek eklemenin alternatifi
 /* for (let i = 0; i<formGroup; i++){
@@ -20,8 +22,14 @@ const formGroup = document.querySelectorAll(".form-group");
 }*/ 
 
 form.addEventListener("submit",(e) =>{
-   //Ekleye'ye basınca çıkacak tablonun doldurulması
-    cars.innerHTML +=
+    //Kullanıcının her alanı doldurmasını belirten yer
+    if(resim.value =="" || model.value == "" || yıl.value == "" || renk.value == "" || km.value == "" || fiyat.value == "")
+    {
+        div();
+    }
+    //Ekleye'ye basınca çıkacak tablonun doldurulması
+    else{
+        cars.innerHTML +=
     `<tr>
         <td><img src="${resim.value}" class="img-fluid img-thumbnail"></td>
             <td>${model.value}</td>
@@ -30,8 +38,12 @@ form.addEventListener("submit",(e) =>{
             <td>${km.value}</td>
             <td>${fiyat.value}<a href="#" id = "delete-car" class = "btn btn-danger">Satıldı</a></td>
     </tr>`
-    e.preventDefault(); 
     clear();
+    document.querySelector(".alert").remove();
+    //div.remove();
+    }   
+    e.preventDefault(); 
+    
 
 })
 //Butona basılınca içeriyi temizlesin
@@ -42,4 +54,12 @@ function clear(){
     renk.value = "";
     km.value = "";
     fiyat.value = "";
+}
+//Boş alan bırakılmaması için gerekli uyarıyı oluşturma
+function div(){
+    const div = document.createElement("div");
+    div.className = "alert alert-success"
+    div.textContent = "Boş alan bırakmayın!"
+    body.appendChild(div);
+
 }
